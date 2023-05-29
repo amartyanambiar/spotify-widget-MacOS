a3836143fd06a7ec612c2a6c42046aa261a599ba
amartyanambiar <amartyanambiar@gmail.com>
2023-05-29 19:15:02 +0530

    Initial Commit

---
+++ spotify-widget/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  spotify-widget
+//
+//  Created by Amartya Nambiar on 29/05/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
